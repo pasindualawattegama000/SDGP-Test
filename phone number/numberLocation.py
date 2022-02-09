5db9fa83b53f502b4efeae3774b329ee68c5b4c6
Pasindu Alawattegama <pasindu.20200732@iit.ac.lk>
2022-02-09 15:17:58 +0530

    Add files via upload

---
+++ phone number/numberLocation.py
@@ -0,0 +1,41 @@
+import phonenumbers
+
+import folium
+
+from myNumber import number
+
+from phonenumbers import geocoder
+
+Key = '3151137254314b12b4eb6f324244bceb'
+
+samNumber = phonenumbers.parse(number)
+
+yourLocation = geocoder.description_for_number(samNumber, "en")
+print(yourLocation)
+
+## get service provider
+
+from phonenumbers import carrier
+
+service_provider = phonenumbers.parse(number)
+print(carrier.name_for_number(service_provider, "en"))
+
+from opencage.geocoder import OpenCageGeocode
+
+geocoder = OpenCageGeocode(Key)
+
+query = str(yourLocation)
+
+results = geocoder.geocode(query)
+# print(results)
+
+lat = results[0]['geometry']['lat']
+
+lng = results[0]['geometry']['lng']
+print(lat,lng)
+
+myMap = folium.Map(location=[lat,lng],zoom_start=9)
+
+folium.Marker([lat,lng],popup=yourLocation).add_to(myMap)
+
+myMap.save("myLocation.html")
